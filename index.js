8e909f3dd81b218c08f9c9b7978d3aac547bb203
chimixgs <159871244+chimixgs@users.noreply.github.com>
2024-02-13 00:14:06 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Si no decias que shi, lloraba :c...te amo, Santiii')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
